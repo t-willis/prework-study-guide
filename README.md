118e4695ec70aefa889861b8a92c8a6619a95ccf
skybreakbb <willistaylora@gmail.com>
2023-04-04 23:25:38 -0500

    updated project README file, added JavaScript for dark mode and day counter, added dark mode to CSS

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# prework-study-guide
-study guide for course prework
+# Prework Study Guide Webpage
+
+## Description
+
+- This webpage is designed primarily to hold various notes and study materials pertinent to my upcoming Web Development coding bootcamp.
+- Building this project was necessary as it is far more readable, navigable, and searchable than simply using a .txt file or physical notepad.
+- Having notes for various subjects in one place, neatly organized, helps streamline the studying process.
+- Aside from the obvious benetit of consolidating notes to a single platform, the construction of the webpage gave an introduction to several subjects, most noteably HTML, CSS and JavaScript.
+
+## Installation
+
+N/A
+
+## Usage
+
+- To use this Study Guide you can review the notes in each section.
+- For a randomized suggestion of what to study first you can opening your browsers integrated DevTools using the default shortcut Control+Shift+I (Windows) or Command+Option+I (macOS).
+- There you will see a list of topics we learned about during the prework as well as a suggestion of which topic to study first.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
+
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
+// Defining variables for topic list
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-
+// Defining function to log each viariable in topics array
 function listTopics() {
   for (var x = 0; x < topics.length; x++) {
     console.log(topics[x])
   }
 }
 
-
+// Defining function to randomly select one of four variables listed in topics array
 function selectTopic() {
 
   if (randomTopic === 'HTML') {
@@ -25,15 +25,27 @@ function selectTopic() {
   }
 }
 
-
+// Printing to console the topics learned and randomly selected topic
 console.log('Here are the topics we learned through Prework:');
 listTopics()
 console.log('Which topic should we study first?');
 selectTopic()
 
+// Dark mode toggle function because my eyes hurt
+function dmToggle() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+// Trying to make a function to count the days I've been learning coding.
+var oneDay = 24 * 60 * 60 * 1000;
+var then = new Date(2023, 3, 1, 12, 0, 0, 0);
+var now = new Date();
+var diffDays = Math.round(Math.abs((then.getTime() - now.getTime()) / oneDay));
+console.log("I have been learning to code for " + diffDays + " days!");
 
 
-/* This is an array with a console.log() method to print the first item in the array
+/* This is an example array with a console.log() method to print the first item in the array
 var shapes = ["triangle", "square", "pentagon", "circle"];
 console.log(shapes[0]);
 */
